070748fa32ce9a4629f9520f6f54681c44f2a6eb
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-12 19:05:08 -0300

    Componente finalizado

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import BuscaCEP from "./src/Components/BuscaCep";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <BuscaCEP />
+    <View  >
+        <BuscaCEP />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
--- src/Components/BuscaCep/index.js
+++ src/Components/BuscaCep/index.js
@@ -1,25 +1,107 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
 import api from "../../services/api";
 
 const BuscaCEP = () => {
-  const [cep, setCep] = useState({});
+  const [dados, setDados] = useState({});
+  const [input, setInput] = useState();
 
-  useEffect(() => {
-    async function buscarCEP() {
-      const response = await api.get("58410367/json/");
-      setCep(response.data);
-    }
-    buscarCEP();
-  }, []);
+  async function buscarCEP() {
+    await api
+      .get(`${input}/json/`)
+      .then((response) => setDados(response.data))
+      .catch((error) => console.log(error));
+  }
 
   return (
-    <View>
-      <Text>{cep.cep}</Text>
-    </View>
+    <SafeAreaView >
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>Digite o CEP desejado</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 51234567"
+          value={input}
+          onChangeText={(texto) => setInput(texto)}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={[styles.btn, { backgroundColor: "#1d75cd"}]} onPress={buscarCEP}>
+          <Text style={styles.btnText}>Buscar CEP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.btn, { backgroundColor: "#cd3e1d"}]} onPress={buscarCEP}>
+          <Text style={styles.btnText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>CEP: {dados.cep}</Text>
+        <Text style={styles.itemText}>Logradouro: {dados.logradouro}</Text>
+        <Text style={styles.itemText}>Complemento: {dados.complemento}</Text>
+        <Text style={styles.itemText}>Bairro: {dados.bairro}</Text>
+        <Text style={styles.itemText}>Cidade: {dados.localidade}</Text>
+        <Text style={styles.itemText}>Estado: {dados.uf}</Text>
+      </View>
+
+    </SafeAreaView>
   );
 };
 
 export default BuscaCEP;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer:{
+    alignItems: 'center'
+  },
+  text: {
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    width: "90%",
+    padding: 10,
+    fontSize: 18,
+  },
+  btnArea: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "space-around",
+  },
+  btn: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 5,
+  },
+  btnText: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  itemContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  itemText:{
+    fontSize: 18
+  }
+});
